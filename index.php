e36826e4702af2fd770c68cf8292ba1f27793f5b
Maggda446 <57993238+Maggda446@users.noreply.github.com>
2020-10-27 14:25:08 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,33 +7,10 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php
-    $servername="127.0.0.1";
-    $username="root";
-    $password="";
-    $dbname="library";
-
-    $conn=new mysqli($servername,$username,$password,$dbname);
-
-    $result=$conn->query("select * from ksiazki");
-    echo("<table border='1'>");
-    echo("<tr>
-    <th>id_autor_tytul</th>
-    <th>name</th>
-    <th>tytul</th>
-    </tr>");
-
-    while($row=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("
-        <td>".$row['id_autor_tytul']."</td>
-        <td>".$row['name']."</td>
-        <td>".$row['tytul']."</td>
-        ");
-        echo("</tr>");
-    };
-    echo("</table>");
-    ?>
+<div class="header"></div>
+<div class="main"></div>
+<div class="sidebar"></div>
+<div class="footer"></div>
 </body>
 <script src="script.js"></script>
 </html>
