1c4630afcaada15b5f2c10fec1e1219861c7470c
Maggda446 <57993238+Maggda446@users.noreply.github.com>
2020-11-09 23:37:11 +0100

    Add files via upload

--- insert_books.php
+++ insert_books.php
@@ -9,19 +9,18 @@
               }
               
            
-    $sql_autor= "INSERT INTO `lib_autorzy`(`name`) VALUES ('".$_POST['name']."')";
-    $sql_tytul= "INSERT INTO `lib_tytuly`(`tytul`) VALUES ('".$_POST['tytul']."')";
+    $sql1= "INSERT INTO `lib_autorzy`(`id_autor`,`name`) VALUES (null,'".$_POST['name']."')";
+    mysqli_query($conn, $sql1);
 
-    mysqli_query($conn, $sql_autor);
-    mysqli_query($conn, $sql_tytul);
+    $sql2= "INSERT INTO `lib_tytuly`(`id_tytul`,`tytul`) VALUES (null,'".$_POST['tytul']."')";
+    mysqli_query($conn, $sql2);
 
-    $sql_a_t= "INSERT INTO `lib_autor_tytul`(`id_autor`, `id_tytul`) VALUES ('".$_POST['id_autor']."','".$_POST['id_tytul']."')";
+    $sql3= "INSERT INTO `lib_autor_tytul`( `id_autor_tytul`,`id_autor`, `id_tytul`) VALUES (null,'".$_POST['id_autor']."','".$_POST['id_tytul']."')";
+    mysqli_query($conn, $sql3);
 
-    mysqli_query($conn, $sql_a_t);
-            
-         
     
-          
+   
+
               header("location:admin.php");
             ?>
 
