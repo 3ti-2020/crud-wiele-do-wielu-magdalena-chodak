cc7e4b73645945a2b7c41449a07d64c847a084f2
Maggda446 <57993238+Maggda446@users.noreply.github.com>
2020-11-26 13:16:59 +0100

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,21 +1,37 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const todoList = document.querySelector('#todoList');
     const todoForm = document.querySelector('#todoForm');
     const todoSearch = document.querySelector('#todoSearch');
-    const todoInput = todoForm.querySelector('input');
+    const todoTextarea = todoForm.querySelector('textarea');
 
     function addTask(text) {
-		console.log("Dodaję zadanie do listy");
-       
+        const element = document.createElement("div");
+        element.classList.add("element");
+
+        //pobieram zawartość templatki
+        const elementInner = document.querySelector("#elementTemplate").content.cloneNode(true);
+
+        //wrzucam do elementu
+        element.append(elementInner);
+
+        //tworzę datę
+        const date = new Date();
+        const dateText = `${date.getDate()} - ${date.getMonth()+1} - ${date.getFullYear()} godz.: ${date.getHours()}:${date.getMinutes()}`;
+        element.querySelector(".element-date").innerText = dateText;
+
+        //wstawiam tekst
+        element.querySelector(".element-text").innerText = text;
+
+        //i wrzucam element do listy
+        todoList.append(element);
     }
 
     todoForm.addEventListener('submit', e => {
         e.preventDefault();
 
-        if (todoInput.value !== '') {
-            addTask(todoInput.value);
-            todoInput.value = '';
+        if (todoTextarea.value !== '') {
+            addTask(todoTextarea.value);
+            todoTextarea.value = '';
         }
     });
 
@@ -25,5 +41,18 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    
+    todoSearch.addEventListener("input", () => {
+        const val = todoSearch.value;
+        const elems = todoList.querySelectorAll(".element");
+
+        for (const el of elems) {
+            const text = el.querySelector(".element-text").innerText;
+
+            if (text.includes(val)) {
+                el.style.setProperty("display", "");
+            } else {
+                el.style.setProperty("display", "none");
+            }
+        }
+    });
 });
