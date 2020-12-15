d2397f706ffbd6854197efc08fd167748e30b923
Maggda446 <57993238+Maggda446@users.noreply.github.com>
2020-12-15 14:52:02 +0100

    Add files via upload

--- blog.php
+++ blog.php
@@ -12,7 +12,7 @@
 <a href="https://github.com/3ti-2020/crud-wiele-do-wielu-magdalena-chodak"><img src="https://miro.medium.com/max/2560/1*JLYlSLSK8-AZo8gt9UdYqA.jpeg"></a>
 </div>
 <div class="title">
-<h1>Magdalena Chodak gr2</h1>
+<h1>BLOG</h1>
 </div> 
 <div class="menu">  
 <A href="index.php">Powrót</a>
@@ -21,62 +21,34 @@
 
 <div class="sidebar">
 <?php
-//  $servername="127.0.0.1";
-//  $username="root";
-//  $password="";
-//  $dbname="library";
-$servername="mysql-magdaa.alwaysdata.net";
-    $username="magdaa";
-    $password="Pocomito123";
-    $dbname="magdaa_lib";
+      $servername="mysql-magdaa.alwaysdata.net";
+      $username="magdaa";
+      $password="Pocomito123";
+      $dbname="magdaa_lib";
 
-    $conn=new mysqli($servername,$username,$password,$dbname);
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
-
-    $result=$conn->query("SELECT id,title,post,tag FROM `tag_post`,`tag`,`post` where tag_post.id_post=post.id_post and tag_post.id_tag=tag.id_tag");
-    echo("<table border='1'>");
-    echo("<tr>
-    <th>id</th>
-    <th>tytul</th>
-    <th>post</th>
-    <th>tag</th>
-    </tr>");
-
-    while($row=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("
-        <td>".$row['id']."</td>
-        <td>".$row['title']."</td>
-        <td>".$row['post']."</td>
-        <td>".$row['tag']."</td>
-        <td>
-        <form action='delete_blog.php' method='POST'>
-        <input type='text'  value=".$row['id']." style='display: none' name='id'>
-        <input type='submit' class='del' value='x'>
-    </form>
-</td>");
-        echo("</tr>");
-    };
-    echo("</table>");
+        $conn = new mysqli($servername, $username, $password, $dbname);
+    
+        $result = $conn->query("SELECT id,title,post,tag FROM `tag_post`,`tag`,`post` where tag_post.id_post=post.id_post and tag_post.id_tag=tag.id_tag");
+       
     ?>
-</div>
-<div class="footer">
 
-   <div class="blog">
-   <h3>Dodaj post:</h3>
-   <form action='insert_blog.php' method='POST'>
-        <br>tytul:
-        <input type='text' name='title'>
-        <br>post:
-        <input type='text' name='post'>
-        <br>tag:
-        <input type='text' name='tag'>
-        <br><input  type='submit' value='DODAJ'>
-    </form>
-   </div>
-   
-</div>
+
+    <div class="posts">
+        <?php
+                while($row = $result->fetch_assoc()){
+                    echo("<div class='tytul'>");
+                        echo("<h1>".$row['title']."</h1>");
+                    echo("</div>");
+                    echo("<div class='tekst'>");
+                        echo("<p>".$row['post']."</p>"); 
+                    echo("</div>");
+                    echo("<div class='tagi'>");
+                    echo("<h3>".$row['tag']."</h3>");
+                echo("</div>"); 
+                }
+               
+        ?>
+    </div>
+
 </body>
 </html>
