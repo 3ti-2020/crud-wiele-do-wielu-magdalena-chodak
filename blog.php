3aa365bffb67abadf2d3a29ffb1b1873a712b1aa
Maggda446 <57993238+Maggda446@users.noreply.github.com>
2020-12-16 09:33:57 +0100

    Add files via upload

--- blog.php
+++ blog.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog</title>
-    <link rel="stylesheet" href="style_blog.css">
+    <link rel="stylesheet" href="styl_blog.css">
 </head>
 <body>
 <div class="header">
@@ -18,20 +18,7 @@
 <A href="index.php">Powrót</a>
 </div>
 </div>
-<div class="menu">
-<div class="dod_post">
-   <h3>Dodaj post:</h3>
-   <form action='insert_blog.php' method='POST'>
-        <br>Tytuł:
-        <input type='text' name='title'>
-        <br>Treść:
-        <input type='text' name='post'>
-        <br>Tag:
-        <input type='text' name='tag'>
-        <br><input  type='submit' value='WSTAW'>
-    </form>
-   </div>
-</div>
+
 <div class="sidebar">
 <?php
       $servername="mysql-magdaa.alwaysdata.net";
@@ -40,26 +27,25 @@
       $dbname="magdaa_lib";
 
         $conn = new mysqli($servername, $username, $password, $dbname);
-    
-        $result = $conn->query("SELECT id,title,post,tag FROM `tag_post`,`tag`,`post` where tag_post.id_post=post.id_post and tag_post.id_tag=tag.id_tag");
-       
+     
     ?>
 
 
     <div class="posts">
         <?php
-                while($row = $result->fetch_assoc()){
+               
+                $result = $conn->query("SELECT Distinct title, post, id_post FROM post"); 
+
+                while($row = $result -> fetch_assoc()){
                     echo("<div class='pole'>");
-                    echo("<div class='tytul'>");
-                        echo("<h1>".$row['title']."</h1>");
+                        echo("<div class='tytul'><h2>".$row['title']."</h2></div>");
+                        echo("<div class='tekst'>".$row['post']."</div>");
+                        $id = $row['id_post'];
+                        $result2 = $conn -> query("SELECT tag FROM `tag_post`,`tag`,`post` where tag_post.id_post=post.id_post and tag_post.id_tag=tag.id_tag AND tag_post.id_post = $id");
+                        while($row2 = $result2 -> fetch_assoc()){
+                            echo("<nav class='tagi'>".$row2['tag']."</nav>");
+                        }
                     echo("</div>");
-                    echo("<div class='tekst'>");
-                        echo("<p>".$row['post']."</p>"); 
-                    echo("</div>");
-                    echo("<div class='tagi'>");
-                    echo("<h3>tagi: ".$row['tag']."</h3>");
-                echo("</div>");
-                echo("</div>"); 
                 }
                
         ?>
